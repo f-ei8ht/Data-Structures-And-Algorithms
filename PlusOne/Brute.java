c02874589e2095efe166ebc1fe35f02373b6a44c
f-ei8ht <saif7862254@outlook.com>
2025-04-15 11:27:10 +0530

    plus one

--- PlusOne/Brute.java
+++ PlusOne/Brute.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] == 9) {
+            if (digits[i] == 9)
                 digits[i] = 0;
-            } else {
+            else {
                 digits[i]++;
                 return digits;
             }
