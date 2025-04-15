816a45950e76ec762c2caceb2e436bd1c53f9574
f-ei8ht <saif7862254@outlook.com>
2025-04-15 11:26:26 +0530

    plus one

--- PlusOne/Brute.java
+++ PlusOne/Brute.java
@@ -1,21 +1,15 @@
-import java.util.Arrays;
-
-public class Brute {
-    public static int[] plusOne(int[] digits) {
-        int temp = 0;
-        int[] newDigits = Arrays.copyOf(digits, digits.length + 1);
-        for (int i = 0; i < digits.length; i++) {
-            if (i == digits.length - 1 && digits[digits.length - 1] < 9) {
-                temp = digits[digits.length - 1];
-                temp = temp + 1;
-                digits[digits.length - 1] = temp;
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
                 return digits;
-            } else if (i == digits.length - 1 && digits[digits.length - 1] == 9) {
-                newDigits[newDigits.length - 2] = 1;
-                newDigits[newDigits.length - 1] = 0;
-                return newDigits;
             }
         }
-        return new int[] {};
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
