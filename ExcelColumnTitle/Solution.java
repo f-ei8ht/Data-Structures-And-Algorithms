b0638420a458495c6e5f07ecf4bec54f674b62e2
f-ei8ht <saif7862254@outlook.com>
2025-07-01 14:58:14 +0530

    Excel Column title

---
+++ ExcelColumnTitle/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        StringBuilder st = new StringBuilder(columnTitle);
+        int sum = 0;
+        int count = 0;
+        st.reverse();
+        for (int i = 0; i < st.length(); i++) {
+            sum = (int) (sum + (Math.pow(26, count++) * (st.charAt(i) - 'A' + 1)));
+        }
+        return sum;
+    }
+}
